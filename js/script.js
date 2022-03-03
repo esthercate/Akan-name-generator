e002acb5d1d747de48857077e70d3f38cb13df5e
esthercate <vuthi.esther@gmail.com>
2022-03-03 21:37:42 +0300

    adds script js for collecting user input

---
+++ js/script.js
@@ -0,0 +1,10 @@
+document.getElementById("myBtn").onclick = function() {
+  var dd = document.getElementById("dateOfBirth").value;
+  var mm = document.getElementById("monthOfBirth").value;
+  var year = document.getElementById("yearOfBirth").value;
+  
+  //function getDay(CC, YY, MM, DD) {
+    //var dow = (((CC/4) -2*CC-1) + ((5*YY/4)) + ((26*(MM+1)/10)) + DD) % 7;
+    //return dow.toFixed();
+  //}
+}
