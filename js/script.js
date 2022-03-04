f564f816f3b04f7547eeee91fa2d5c3f90418249
esthercate <vuthi.esther@gmail.com>
2022-03-04 12:24:26 +0300

    adds a function to get user gender from radio button

--- js/script.js
+++ js/script.js
@@ -1,13 +1,27 @@
+function getName() {
+  let dd = document.getElementById("dateOfBirth").value;
+  let mm = document.getElementById("monthOfBirth").value;
+  let year = document.getElementById("yearOfBirth").value;
+  let gen = document.getElementsByName("gender").value;
 
-document.getElementById("myBtn").onclick = function() {
-  var dd = document.getElementById("dateOfBirth").value;
-  var mm = document.getElementById("monthOfBirth").value;
-  var year = document.getElementById("yearOfBirth").value;
-  var sex = document.getElementsByName("gender").value;
-  var dow = dd + mm + year + sex;
-  console.log(dow);
-}
+  let cc = Number(year.slice(0,2));
+  let yy = Number(year.slice(2,4));
 
-function getAkanName() {
+
+  function genderInput() {
+    for(let gender of gen) {
+      if(gender.checked){
+        return gender.value;
+      }
+    }
+  }
+
+  var genderValue = genderInput();
+  console.log(genderValue);
+
+  //calculating day of week
+
+  var dayOfWeek = (((cc/4) -2*cc-1) + ((5*yy/4) ) + ((26*(mm+1)/10)) + dd ) % 7;
+
 
 }
