e5393b2c0caf787f662492133f435ce0169d4ea4
esthercate <vuthi.esther@gmail.com>
2022-03-04 09:01:22 +0300

    add getAkanName function

--- js/script.js
+++ js/script.js
@@ -1,10 +1,13 @@
+
 document.getElementById("myBtn").onclick = function() {
   var dd = document.getElementById("dateOfBirth").value;
   var mm = document.getElementById("monthOfBirth").value;
   var year = document.getElementById("yearOfBirth").value;
-  
-  //function getDay(CC, YY, MM, DD) {
-    //var dow = (((CC/4) -2*CC-1) + ((5*YY/4)) + ((26*(MM+1)/10)) + DD) % 7;
-    //return dow.toFixed();
-  //}
+  var sex = document.getElementsByName("gender").value;
+  var dow = dd + mm + year + sex;
+  console.log(dow);
+}
+
+function getAkanName() {
+
 }
