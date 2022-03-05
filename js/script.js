49a29e13d5d8cc62b41d4811a4f11adba8c7b28b
esthercate <vuthi.esther@gmail.com>
2022-03-05 09:18:55 +0300

    adds an if statement to l0op through day of the week and gender

--- js/script.js
+++ js/script.js
@@ -1,53 +1,25 @@
-function getName() {
-  let dd = document.getElementById("dateOfBirth").value;
-  let mm = document.getElementById("monthOfBirth").value;
-  let year = document.getElementById("yearOfBirth").value;
-  //let gender = document.getElementsByName("gender").value;
-
-  let cc = Number(year.slice(0,2));
-  let yy = Number(year.slice(2,4));
-
-   //calculating day of week
-
-   let dayOfWeek = (((cc/4) -2*cc-1) + ((5*yy/4) ) + ((26*(mm+1)/10)) + dd ) % 7;
-
-   document.getElementById("result").textContent = "You were born on " + dayOfWeek;
-
-
- /* function genderInput() {
-      if(gender.checked === true){
-        return gender.value;
-      }
-  }
-
-  let genderValue = genderInput();
-  console.log(genderValue);
-
-  //Arrays to store akan names and days of the week
-
-  let daysOfTheWeek = ["sunday", "monday", "tuesday", "wednesday", "thursday", "friday", "saturday"];
-
-  let maleNames = ["Kwasi", "Kwadwo", "Kwabena", "Kwaku", "Yaw", "Kofi", "Kwame"];
-
-  let femaleNames = ["Akosua", "Adwoa", "Abenaa", "Akua", "Yaa", "Afua", "Ama"];
-
-  //looping through arrays
-
-  let i;
-  if(dayOfWeek == 0) {
-    i = 6;
+ var akan = function(y, m, d, g) {
+  var femaleNames = ["Akosua", "Adwoa", "Abenaa", "Akua", "Yaa", "Afua", "Ama"];
+  var maleNames = ["Kwasi", "Kudwo", "Kwabena", "Kwaku", "Yaw", "Kofi", "Kwame"];
+  
+  var d = new Date(y, --m, d);
+  if (g === "Female") {
+      return d && femaleNames[d.getDay()];
   } else {
-    i = dayOfWeek - 1;
+      return d && maleNames[d.getDay()];
   }
-  console.log(i);
-
-  if(genderValue == "male" && mm && dd) {
-    document.getElementById("result").textContent = "Your Akan Name is " + maleNames[i];
-    return false;
-  } else if(genderValue == "female" && mm && dd) {
-    document.getElementById("result").textContent = "Your Akan Name is " + femaleNames[i];
-    return false;
-  } else {
-    alert("The data you entered is invalid!")
-  }*/
 }
+
+$(document).ready(function() {
+  $("form#form").submit(function(event) {
+      event.preventDefault();
+      var y = parseInt($("#yearOfBirth").val());
+      var m = parseInt($("#monthOfBirth").val());
+      var d = parseInt($("#dateOfBirth").val());
+      var g = $("input:radio[name=gender]:checked").val();
+      var result = akan(y, m, d, g);
+      alert("Your akan name is: " + result);
+      //refresh page
+      document.getElementById("form").reset();
+  });
+}); 
